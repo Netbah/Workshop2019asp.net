c38f658cbb5d0a39562862f0faa52de579cecd3b
Sergei Bakhtoiarov <sergey.bakhtoyarov@gmail.com>
2019-05-21 15:52:35 +0200

    cashe

--- lab/BackEnd/LogContext.cs
+++ lab/BackEnd/LogContext.cs
@@ -8,14 +8,23 @@ namespace BackEnd
 {
     public class LogContext : ILogContext
     {
-        private readonly HttpContext _httpContext;
-        public LogContext(HttpContext context)
-        {
-            _httpContext = context;
-        }
-        public string Path { get { return _httpContext.Request.Path; } }
+        // private readonly HttpContext _httpContext;
+        // public LogContext(HttpContext context)
+        // {
+        //     _httpContext = context;
+        // }
+        // public string Path { get { return _httpContext.Request.Path; } }
 
-        public string TraceIdentifier { get { return _httpContext.TraceIdentifier; } }
+        // public string TraceIdentifier { get { return _httpContext.TraceIdentifier; } }
+
+        public LogContext(HttpContext httpContext)
+        {
+            Path = httpContext.Request.Path;
+            TraceIdentifier = httpContext.TraceIdentifier;
+        }
+        public string Path { get; }
+
+        public string TraceIdentifier { get; }
     }
 
     public interface ILogContext
